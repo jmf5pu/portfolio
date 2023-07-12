cd46919fe749fc06ac6739ba79010dba75498154
jmf5pu <59934098+jmf5pu@users.noreply.github.com>
2023-07-11 21:25:03 -0400

    playing with components

---
+++ src/components/MainWindow.tsx
@@ -0,0 +1,10 @@
+import styles from "./styles/MainWindow.module.css";
+
+function MainWindow() {
+  return (
+    <div className={styles.parentDiv}>
+      <h1>this is the main window</h1>
+    </div>
+  );
+}
+export default MainWindow;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,10 @@
+import styles from "./styles/NavBar.module.css";
+
+function NavBar() {
+  return (
+    <div className={styles.parentDiv}>
+      <h1>this is the top navigation bar</h1>
+    </div>
+  );
+}
+export default NavBar;
