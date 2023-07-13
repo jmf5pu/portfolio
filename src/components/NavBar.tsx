6d57199127f7de0a9228ead9ec585abc49a406ab
jmf5pu <59934098+jmf5pu@users.noreply.github.com>
2023-07-13 15:13:04 -0400

    add basic tab layout, state hook

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,32 @@
 import styles from "./styles/NavBar.module.css";
+import NavBarTab from "./NavBarTab";
+import { useState } from "react";
 
 function NavBar() {
+  const [activeTab, setActiveTab] = useState(1);
   return (
     <div className={styles.parentDiv}>
-      <h1>this is the top navigation bar</h1>
+      <NavBarTab
+        activeTab={activeTab}
+        onClick={() => {
+          setActiveTab(1);
+        }}
+        tabNum={1}
+      />
+      <NavBarTab
+        activeTab={activeTab}
+        onClick={() => {
+          setActiveTab(2);
+        }}
+        tabNum={2}
+      />
+      <NavBarTab
+        activeTab={activeTab}
+        onClick={() => {
+          setActiveTab(3);
+        }}
+        tabNum={3}
+      />
     </div>
   );
 }
---
+++ src/components/NavBarTab.tsx
@@ -0,0 +1,23 @@
+import styles from "./styles/NavBarTab.module.css";
+
+interface NavBarTabProps {
+  activeTab: number;
+  onClick: () => void;
+  tabNum: number;
+}
+
+function NavBarTab(props: NavBarTabProps) {
+  return (
+    <div
+      className={
+        props.activeTab === props.tabNum
+          ? styles.tabSelected
+          : styles.tabNotSelected
+      }
+      onClick={props.onClick}
+    >
+      <h1>tab 1</h1>
+    </div>
+  );
+}
+export default NavBarTab;
